e5586c797e4c47ab2cfbeb8852b5f44e850422e2
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-08-09 11:28:25 +0530

    Ahe

--- src/ComponentCart/Checkout.js
+++ src/ComponentCart/Checkout.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Checkout = ({cart,cartSize}) => {
   return (
-    <div>Checkout</div>
+    <>
+    <div className='containerCheckout' style={checkout}>
+    <h1 style={{margin:"20vh auto",textAlign:"center",fontSize:"6vh"}}>{cartSize} Products Ordered</h1>
+    <h1 style={{textAlign:"center"}}>Thank You for Ordering</h1>
+    </div>
+    </>
   )
 }
 
-export default Checkout
+export default Checkout;
+
+const checkout = {
+  position:"absolute",
+  top:"0",
+  right:2,
+  left:0,
+  height:"100vh",
+  background:"url('https://www.psdtemplatesblog.com/wp-content/uploads/2015/07/Shopify-Checkout.gif')",
+  backgroundRepeat:"no-repeat",
+  backgroundPosition:"bottom",
+  backgroundSize:"contain",
+}
